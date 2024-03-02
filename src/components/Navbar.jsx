2db14e95bd575f106f50189be5182bf6d9b4e9ca
navjotNSK <navjotsingh.uiet@gmail.com>
2024-03-02 23:24:07 +0530

    Refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react';
-import trollface from '../assets/trollface.png';
-import '../App.css';
-
+import { useState } from "react";
+import trollface from "../assets/trollface.png";
+import "../App.css";
 
 function Navbar() {
-    const [count, setCount] = useState(0)
-  
-    return (
-      
-      <>
-       
-   <nav className="header">
-   <img src={trollface} className="header--image"alt="Header logo" />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
-</nav>
-   </>
-    )
-  }
+  const [count, setCount] = useState(0);
 
-  export default Navbar
+  return (
+    <>
+      <nav className="header">
+        <img src={trollface} className="header--image" alt="Header logo" />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+      </nav>
+    </>
+  );
+}
 
-  
+export default Navbar;
